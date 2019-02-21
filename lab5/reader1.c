42c47196e3f453b7064a2cf5ffcaffac3df0cab3
chadmv95 <chadmv95@gmail.com>
2019-02-20 23:05:00 -0500

    save it

--- lab5/reader1.c
+++ lab5/reader1.c
@@ -17,26 +17,29 @@ void shutDownHandler(int);
 int main() 
 { 
     // ftok to generate unique key 
-    key_t key = ftok("shmfile",65);
+    key_t key1 = ftok("shmfile",65); 
+    // key_t key2 = ftok("shmfile2",65);
+    // key_t key3 = ftok("shmfile3",65);
 
     // shmget returns an identifier in shmid 
-    int shmid1 = shmget(key,1024,0666|IPC_CREAT); 
-    int shmid2 = shmget(key,1024,0666|IPC_CREAT); 
-    int shmid3 = shmget(key,1024,0666|IPC_CREAT); 
+    int shmid1 = shmget(key1,4096,0666|IPC_CREAT); 
+    // int shmid2 = shmget(key2,4096,0666|IPC_CREAT); 
+    // int shmid3 = shmget(key3,4096,0666|IPC_CREAT); 
   
     // shmat to attach to shared memory 
-    str = (char*) shmat(shmid1, (void*)0, SHM_RDONLY);
-    turn = (int*) shmat(shmid2, (void*)0, 0);
-    flag = (int*) shmat(shmid3, (void*)0, 0);
+    str = (char*) shmat(shmid1, (void*)0, 0);
+    // turn = (int*) shmat(shmid2, (void*)0, 0);
+    // flag = (int*) shmat(shmid3, (void*)0, 0);
 
     do{
-        *(flag+1) = 1; //sets flag[1] to 1
-        *turn = 0;
-        while(*(flag) == 1 && *turn == 0);
-        printf("Data read from memory: %s\n",str);
-        *(flag+1) = 0; //sets flag[1] to 0
+        // *(flag+1) = 1; //sets flag[1] to 1
+        // *turn = 0;
+        // while(*(flag) == 1 && *turn == 0);
+    printf("im here\n");
+        printf("Data read from memory: %s\n", str);
+        // *(flag+1) = 0; //sets flag[1] to 0
 
-    }while(0);
+    }while(1);
 
 
 
@@ -46,8 +49,8 @@ int main()
 void shutDownHandler (int sigNum) {
     //detach from shared memory  
     shmdt(str); 
-    shmdt(turn);
-    shmdt(flag);
+    // shmdt(turn);
+    // shmdt(flag);
 
 	printf(" Okay I'm shutting you down.\n");
 	exit(0);
--- lab5/writer.c
+++ lab5/writer.c
@@ -1,5 +1,7 @@
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h> 
 #include <stdlib.h>
 #include <signal.h>
@@ -19,31 +21,36 @@ int main()
     signal(SIGINT, shutDownHandler);
 
     // ftok to generate unique key 
-    key_t key = ftok("shmfile",65); 
+    key_t key1 = ftok("shmfile1",65); 
+    // key_t key2 = ftok("shmfile2",65);
+    // key_t key3 = ftok("shmfile3",65);
+
   
     // shmget returns an identifier in shmid 
-    int shmid1 = shmget(key,1024,0666|IPC_CREAT); 
-    int shmid2 = shmget(key,1024,0666|IPC_CREAT); 
-    int shmid3 = shmget(key,1024,0666|IPC_CREAT); 
+    int shmid1 = shmget(key1,1024, 0666|IPC_CREAT); 
+    // int shmid2 = shmget(key2,1024, 0666|IPC_CREAT); 
+    // int shmid3 = shmget(key3,1024, 0666|IPC_CREAT); 
   
     // shmat to attach to shared memory 
-    str = (char*) shmat(shmid1,(void*)0,0);
-    turn = (int*) shmat(shmid2, (void*)0,0);
-    flag = (int*) shmat(shmid3, (void*)0,0);
+    str = (char*) shmat(shmid1, (void*)0,0);
+    // turn = (int*) shmat(shmid2, (void*)0,0);
+    // flag = (int*) shmat(shmid3, (void*)0,0);
 
-    *(flag) = 0;
-    *(flag+1) = 0;
-    printf("\nFlag:\n%d\t%d\n", *(flag), *(flag+1));
+    // *(flag) = 0;
+    // *(flag+1) = 0;
+    // *turn = 1;
+    // printf("\nFlag:\n%d\t%d\n", *(flag), *(flag+1));
+    // printf("Turn:\n%d\n", *turn);
 
 
     do{
-        *(flag) = 1; //sets flag[0] to 1
-        *turn = 1;
-        while(*(flag+1) == 1 && *turn == 1);
+        // *(flag) = 1; //sets flag[0] to 1
+        // *turn = 1;
+        // while(*(flag+1) == 1 && *turn == 1);
         printf("\nWrite Data:  ");
         fgets(str, STR_LEN, stdin);
         printf("Data written in memory: %s\n",str);
-        *(flag) = 0; //sets flag[0] to 0
+        // *(flag) = 0; //sets flag[0] to 0
     }while(1);
       
 
@@ -54,12 +61,12 @@ int main()
 void shutDownHandler (int sigNum) {
     //detach from shared memory  
     shmdt(str);
-    shmdt(turn);
-    shmdt(flag);
-    // destroy the shared memory 
+    // shmdt(turn);
+    // shmdt(flag);
+    // // destroy the shared memory 
     shmctl(shmid1,IPC_RMID,NULL); 
-    shmctl(shmid2,IPC_RMID,NULL);
-    shmctl(shmid3,IPC_RMID,NULL);
+    // shmctl(shmid2,IPC_RMID,NULL);
+    // shmctl(shmid3,IPC_RMID,NULL);
 
 	printf(" Okay I'm shutting you down.\n");
 	exit(0);
