ca100e908a1e2aaae61cdb77e087089c131fb0ba
chadmv95 <chadmv95@gmail.com>
2019-03-28 11:53:15 -0400

    end of lab time

---
+++ lab9/example1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COEFFICIENT 64
+#define KB 1024
+#define LOOP 1
+
+int main() {
+   int count, *intPtr;
+
+   long int i, j, dim = COEFFICIENT * KB;
+
+   if ((intPtr = malloc (dim * dim * sizeof(int))) == 0) {
+      perror ("totally out of space");
+      exit (1);
+   }
+   //printf("Starting loop :P\n");
+   for (count=1; count<=LOOP; count++)
+      for (i=0; i<dim; i++)
+         for (j=0; j<dim; j++)
+            intPtr[i * dim + j] = (i + j) % count;
+
+   free (intPtr);
+   return 0;
+}
